72d9560ba60828d8b9617eb613f9497697fa4dd0
Georgina <georginamillard2@outlook.com>
2025-10-29 16:57:13 +0000

    Add files via upload

---
+++ BankApp/BudgetService.java
@@ -0,0 +1,12 @@
+package com.mycompany.budgetapp.service;
+
+
+
+public interface BudgetService {
+
+    void deposit(double amount);
+
+    boolean withdraw(double amount);
+
+    double getBalance();
+}
